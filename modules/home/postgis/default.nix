dc459fd421e49aeebaa365327dbb9edc0ae49a9d
bhillermann <bhillermann@gmail.com>
2025-11-18 09:24:39 +1100

    Moved postgis module out of container folder directly into modules folder.

---
+++ modules/home/postgis/default.nix
@@ -0,0 +1,45 @@
+{ 
+  config, 
+  lib, 
+  pkgs, 
+  inputs, 
+  ... 
+}:
+
+
+let
+  postgresUser = "gisuser";
+  postgresPassword = "'$(cat ${config.programs.onepassword-secrets.secretPaths."postgisPassword"})'";
+  postgresDb = "gisdb";
+  dataDir = "/home/brendon/Development/docker-builds/postgis/data/postgis";
+in {
+
+  options = {
+    postgis = {
+      enable = lib.mkOption {
+        description = "Enable postgis as a podman systemd service.";
+        type = lib.types.bool;
+        default = false;
+      };
+    };
+  };
+
+  config = lib.mkIf config.postgis.enable {
+
+    systemd.user.services.postgis = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.podman}/bin/podman run --rm --name postgis \
+          -p 5432:5432 \
+          -v ${dataDir}:/var/lib/postgresql/data \
+          -e POSTGRES_USER=${postgresUser} \
+          -e POSTGRES_PASSWORD=${postgresPassword} \
+          -e POSTGRES_DB=${postgresDb} \
+          docker.io:postgis/postgis";
+        Restart = "always";
+      };
+    };
+
+
+  };
+}
