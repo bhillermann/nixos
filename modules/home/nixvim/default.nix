53dd3792bf0f1f9906b75d88959f5655a2ef00d8
bhillermann <bhillermann@gmail.com>
2025-12-01 13:16:58 +1100

    Formatting.

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -1,34 +1,34 @@
 { lib, pkgs, config, ... }:
 
 {
-    options = {
-        nixvim = {
-            enable = lib.mkOption {
-                description = "Enable NixVim.";
-                type = lib.types.bool;
-                default = false;
-            };
-        };
+  options = {
+    nixvim = {
+      enable = lib.mkOption {
+        description = "Enable NixVim.";
+        type = lib.types.bool;
+        default = false;
+      };
     };
+  };
 
-    config = lib.mkIf config.nixvim.enable {
+  config = lib.mkIf config.nixvim.enable {
 
-        # NVIM setup
-        programs.nixvim = {
-            enable = true;
-            viAlias = true;
-            vimAlias = true;
+    # NVIM setup
+    programs.nixvim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
 
-            opts = {
-                number = true;
-                shiftwidth = 2;
-                completeopt = [ "menu" "menuone" "noselect" ];
-                termguicolors = true;
-            };
+      opts = {
+        number = true;
+        shiftwidth = 2;
+        completeopt = [ "menu" "menuone" "noselect" ];
+        termguicolors = true;
+      };
 
-            colorschemes.catppuccin.enable = true;
-            plugins.lualine.enable = true;
-            plugins.guess-indent.enable = false;
-        };
+      colorschemes.catppuccin.enable = true;
+      plugins.lualine.enable = true;
+      plugins.guess-indent.enable = false;
     };
+  };
 }
