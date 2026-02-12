809174322d7fb3d08fafcade2eca4291c6dd3b30
bhillermann <bhillermann@gmail.com>
2026-02-12 12:03:25 +1100

    Updated config file and env var for nvrmap-web. Had to update db-nvrmap flake input.

--- modules/home/nvrmap-web/default.nix
+++ modules/home/nvrmap-web/default.nix
@@ -1,13 +1,23 @@
 { config, lib, pkgs, inputs, ... }:
 
 let
-  nvrmapConfigPath = "${config.home.homeDirectory}/.config/nvrmap/config.json";
+  nvrmapConfigPath = "${config.home.homeDirectory}/.config/nvrmap/";
+
+  postgresUser = "gisuser";
+  postgresDb = "gisdb";
+  evc_data =
+    "/home/brendon/Documents/GIS/Ensym/EVC benchmark data - external use.xlsx";
+  project = "Scenario Test";
+  collector = "VegLink";
+  default_gain_score = 0.22;
+  default_habitat_score = 0.4;
+
   postgresSecretPath =
     "${config.home.homeDirectory}/.config/opnix/secrets/postgisPassword";
 
   startScript = pkgs.writeShellScript "start-nvrmap-web" ''
     export NVRMAP_CONFIG=${nvrmapConfigPath}
-    export NVRMAP_SECRET=`${pkgs.coreutils}/bin/cat ${postgresSecretPath}`
+    export NVRMAP_DB_PASSWORD=`${pkgs.coreutils}/bin/cat ${postgresSecretPath}`
 
     ${pkgs.db-nvrmap}/bin/db-nvrmap --web --production --host 0.0.0.0 --port 8080
   '';
@@ -23,6 +33,23 @@ in {
   };
 
   config = lib.mkIf config.nvrmap-web.enable {
+
+    home.file."${nvrmapConfigPath}config.json".text = builtins.toJSON {
+      db_connection = {
+        db_type = "postgresql+psycopg2";
+        username = postgresUser;
+        host = "localhost";
+        database = postgresDb;
+      };
+      evc_data = evc_data;
+      attribute_table = {
+        project = project;
+        collector = collector;
+        default_gain_score = default_gain_score;
+        default_habitat_score = default_habitat_score;
+      };
+    };
+
     systemd.user.services.nvrmap-web = {
       Unit = { Description = "Run db-nvrmap web service on 0.0.0.0"; };
       Service = {
