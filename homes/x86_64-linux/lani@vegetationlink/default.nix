bacaa219a02b23eaaaab8df89cf392a912621215
bhillermann <bhillermann@gmail.com>
2026-03-10 15:13:13 +1100

    Corrected lani's profile. program.bash.enable = true was needed to get the env vars to take.

--- homes/x86_64-linux/lani@vegetationlink/default.nix
+++ homes/x86_64-linux/lani@vegetationlink/default.nix
@@ -4,9 +4,11 @@
   imports = [ ];
 
   home = {
-    username = "brendon";
-    homeDirectory = "/home/brendon";
+    username = "lani";
+    homeDirectory = "/home/lani";
   };
+
+  programs.bash.enable = true;
 
   home.sessionVariables = {
     MS_APP_ID =
@@ -17,4 +19,16 @@
       "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msTenantID)";
   };
 
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
