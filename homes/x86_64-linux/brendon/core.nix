6f330dbbaec68f40b1a47710db0a28274fa96d61
bhillermann <bhillermann@gmail.com>
2026-03-12 10:24:25 +1100

    Moved ms secrets to brendon@vegetationlink from core

--- homes/x86_64-linux/brendon/core.nix
+++ homes/x86_64-linux/brendon/core.nix
@@ -8,15 +8,6 @@
     homeDirectory = "/home/brendon";
   };
 
-  home.sessionVariables = {
-    MS_APP_ID =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppID)";
-    MS_APP_SECRET =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppSecret)";
-    MS_TENANT_ID =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msTenantID)";
-  };
-
   # Enable OpNix for Home Manager
   programs.onepassword-secrets = {
     enable = true;
@@ -41,27 +32,6 @@
           "op://nixos-services/nixos-wsl-ssh-private/private key?ssh-format=openssh";
         path = ".ssh/id_ed25519";
         mode = "0600";
-      };
-      msTenantID = {
-        reference = "op://nixos-services/receipt_tracker_api/tenant_id";
-        path = "/mnt/shares/contact_db/secrets/msTenantID";
-        owner = "brendon";
-        group = "users";
-        mode = "0440";
-      };
-      msAppID = {
-        reference = "op://nixos-services/receipt_tracker_api/username";
-        path = "/mnt/shares/contact_db/secrets/msAppID";
-        owner = "brendon";
-        group = "users";
-        mode = "0440";
-      };
-      msAppSecret = {
-        reference = "op://nixos-services/receipt_tracker_api/password";
-        path = "/mnt/shares/contact_db/secrets/msAppSecret";
-        owner = "brendon";
-        group = "users";
-        mode = "0440";
       };
     };
   };
