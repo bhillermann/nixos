e6e6f42631bddcea853e36f9fb78c80fb85bf92c
bhillermann <bhillermann@gmail.com>
2026-03-26 12:13:19 +1100

    Added port 5000 in firewall for contact-web app.

--- systems/x86_64-linux/vegetationlink/default.nix
+++ systems/x86_64-linux/vegetationlink/default.nix
@@ -286,7 +286,7 @@ in {
   };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 5432 8080 8100 ];
+  networking.firewall.allowedTCPPorts = [ 22 5000 5432 8080 8100 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
