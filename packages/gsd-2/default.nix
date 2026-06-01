a97f0244a93c5b795b27c255b98f4606ec0b3546
bhillermann <bhillermann@gmail.com>
2026-06-01 15:13:14 +1000

    Changed to buildNpmPackage custom package. Fetch from git didn't work. Can't edit the appropriate files.

--- packages/gsd-2/default.nix
+++ packages/gsd-2/default.nix
@@ -1,17 +1,27 @@
 { lib, pkgs, stdenv, ... }:
 
-pkgs.buildNpmPackage rec {
-  pname = "gsd-pi";
-  version = "2.43.0";
+let
+  packageLock = pkgs.writeText "gsd-pi-package-lock.json"
+    (builtins.readFile ./package-lock.json);
 
-  src = pkgs.fetchFromGitHub {
-    owner = "gsd-build";
-    repo = "gsd-2";
-    rev = "v${version}";
-    hash = "sha256-mZKrtbN3CZJslIvAiZhQ2drSigiR90qNpmsZo6l4WII=";
+in pkgs.buildNpmPackage rec {
+  pname = "gsd-pi";
+  version = "3.0.0";
+
+  src = pkgs.fetchurl {
+    url = "https://registry.npmjs.org/gsd-pi/-/gsd-pi-${version}.tgz";
+    hash = "sha256:0907hiz1h5r5k7wqmskqi3pgbzahc7xn24rxj9w8wpnrvkrc28lj";
   };
 
-  npmDepsHash = "sha256-4ee6ZhitWCbAAMOBnwXEaKb9Wy4hBeMu2UyfFVPRyF8=";
+  npmDepsHash = "sha256-fd9Gbdbqo38in3lXmnN87cuvv2Jp0fksXzsfQf0Jv5I=";
+
+  prePatch = ''
+    cp ${packageLock} package-lock.json
+  '';
+
+  dontNpmBuild = true;
+
+  npmInstallFlags = [ "--ignore-scripts" ];
 
   meta = with lib; {
     description = "GSD v2 - autonomous coding agent built on the Pi SDK";
