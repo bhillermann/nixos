71fefc43605775879e7de8e43d5ce6e8513abd89
bhillermann <bhillermann@gmail.com>
2026-08-18 12:22:44 +1000

    Updated onepassword reference to 0365 credentials.

--- homes/x86_64-linux/brendon@vegetationlink/default.nix
+++ homes/x86_64-linux/brendon@vegetationlink/default.nix
@@ -1,36 +1,39 @@
-{ lib, pkgs, inputs, config, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
 
 {
   imports = [ ../brendon/core.nix ];
 
   home.sessionVariables = {
-    MS_APP_ID =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppID)";
-    MS_APP_SECRET =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppSecret)";
-    MS_TENANT_ID =
-      "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msTenantID)";
+    MS_APP_ID = "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppID)";
+    MS_APP_SECRET = "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msAppSecret)";
+    MS_TENANT_ID = "$(${pkgs.coreutils}/bin/cat /mnt/shares/contact_db/secrets/msTenantID)";
   };
 
   # Enable OpNix for Home Manager
   programs.onepassword-secrets = {
     secrets = {
       msTenantID = {
-        reference = "op://nixos-services/receipt_tracker_api/tenant_id";
+        reference = "op://nixos-services/o365_app_credentials/tenant_id";
         path = "/mnt/shares/contact_db/secrets/msTenantID";
         owner = "brendon";
         group = "users";
         mode = "0640";
       };
       msAppID = {
-        reference = "op://nixos-services/receipt_tracker_api/username";
+        reference = "op://nixos-services/o365_app_credentials/app_id";
         path = "/mnt/shares/contact_db/secrets/msAppID";
         owner = "brendon";
         group = "users";
         mode = "0640";
       };
       msAppSecret = {
-        reference = "op://nixos-services/receipt_tracker_api/password";
+        reference = "op://nixos-services/o365_app_credentials/app_secret";
         path = "/mnt/shares/contact_db/secrets/msAppSecret";
         owner = "brendon";
         group = "users";
